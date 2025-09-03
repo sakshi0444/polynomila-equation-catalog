6b2e24777e46d536e0a5915fa61f1d1480fedd00
Sakshi Dosaya <123798632+sakshi0444@users.noreply.github.com>
2025-09-03 12:12:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-// Name: Ganta Venkata Kousik
-// Roll No: 21pa1a0549
+
 
 #include <iostream>
 #include <vector>
@@ -130,4 +129,5 @@ int main() {
     processTestCase("input1.json");
     processTestCase("input2.json");
     return 0;
+
 }
